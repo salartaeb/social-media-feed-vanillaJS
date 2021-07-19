422122c0b30769b83216cd3a10681ab5b9b492b9
Salar <salar.taeb@hotmail.com>
2021-07-19 02:06:25 -0700

    declaring more elements and fixing naming errors

--- app.js
+++ app.js
@@ -1,54 +1,83 @@
-const container = document.querySelector('.container');
-const form = document.querySelector('.form');
-const submitPost = document.querySelector('submit-post');
-const likeButton = document.querySelector('like-button');
 const alert = document.querySelector('.alert');
+const container = document.querySelector('.container');
+const form = document.querySelector('.post-form');
+const postBtn = document.querySelector('.submit-post-btn');
+const likeButton = document.querySelector('.like-btn');
 const feed = document.querySelector('.news-feed');
+const postContainer = document.querySelector('.post-container');
   
-const postData = [ 
-  { 
-  "user": {
-    "name": "Salar Taeb",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@Salar12313",
-    },
-  "content": {
-      "text": "this is a post with an image",
-      "image": "https://i.imgur.com/73hZDYK.png",
-    },
-  "created_at": 1461116232227
-},
-{ 
-  "user": {
-    "name": "John Doe",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@JohnDoe",
-    },
-  "content": {
-      "text": "This is a post without an image",
-      "image": ""
-    },
-  "created_at": 1461116232227
-},
-];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const postData = [ 
+//   { 
+//   "user": {
+//     "name": "Salar Taeb",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@Salar12313",
+//     },
+//   "content": {
+//       "text": "this is a post with an image",
+//       "image": "https://i.imgur.com/73hZDYK.png",
+//     },
+//   "created_at": 1461116232227
+// },
+// { 
+//   "user": {
+//     "name": "John Doe",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@JohnDoe",
+//     },
+//   "content": {
+//       "text": "This is a post without an image",
+//       "image": ""
+//     },
+//   "created_at": 1461116232227
+// },
+// ];
 
 // Populate news Feed
 
-fetch(postData)
-  .then((res) => {
-    console.log(res);
-    return res.json();
-  })
-  .then((data) => {
-    return appendData(data);
-  })
-  .catch((err) => {
-    
-  });
-
-  const appendData = (data) => {
-    for (i = 0; i < data.length; i++) {
-      div.innerHTML = 'Name:' + data[i].name;
-      feed.appendChild(div);
-    }
-  }
